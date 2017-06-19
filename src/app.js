9b92edafe5b8c2d65ae88376fb568a527ba1b836
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2017-06-19 17:34:00 +0200

    add editorconfig

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 export class App {
-  
+
   constructor() {
     this.books = [];
     this.bookTitle = "";
   }
 
-  addBook () {
-    this.books.push({title: this.bookTitle});
+  addBook() {
+    this.books.push({ title: this.bookTitle });
     this.bookTitle = "";
     console.log("Book list ", this.books);
   }
