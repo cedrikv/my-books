748d74f66bcedd3aa0612a2079f41208a3322721
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2017-06-20 12:04:27 +0200

    2.7 reorganise files

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ export class App {
     this.router = router;
     config.title = 'My-Books';
     config.map([
-      { route: ['', 'home'], name: 'home', moduleId: 'index' },
-      { route: 'books', name: 'books', moduleId: 'books' },
+      { route: ['', 'home'], name: 'home', moduleId: './resources/elements/index' },
+      { route: 'books', name: 'books', moduleId: './resources/elements/books' },
     ]);
   }
 }
